34ab0ff8cfe45b24b1dcf3200b3ac4bce48c018b
Matthijs Kooijman <matthijs@stdin.nl>
2015-03-02 20:05:48 +0100

    Put field names in PROGMEM
    This saves a few hundred bytes of RAM. This still doesn't allow to run
    the parse example on a Uno, though, it seems that still requires a lot
    of stack space somehow.

--- src/dsmr/fields.h
+++ src/dsmr/fields.h
@@ -175,7 +175,8 @@ const uint8_t SLAVE_MBUS_ID = 4;
     value_t fieldname; \
     bool fieldname ## _present = false; \
     static constexpr ObisId id = obis; \
-    static constexpr char name[] = #fieldname; \
+    static constexpr char name_progmem[] PROGMEM = #fieldname; \
+    static constexpr const __FlashStringHelper *name = reinterpret_cast<const __FlashStringHelper*>(&name_progmem); \
     value_t& val() { return fieldname; } \
     bool& present() { return fieldname ## _present; } \
   }
