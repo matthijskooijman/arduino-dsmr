490233c8f1d07a8ed3fd38fa10626916d6951479
Matthijs Kooijman <matthijs@stdin.nl>
2020-07-01 17:07:45 +0200

    Whitespace fix

--- src/dsmr/crc16.h
+++ src/dsmr/crc16.h
@@ -78,7 +78,7 @@ static inline uint16_t _crc_ccitt_update (uint16_t crc, uint8_t data)
 	data ^= (crc & 255);
 	data ^= data << 4;
 
-	return ((((uint16_t)data << 8) | (crc >> 8)) ^ (uint8_t)(data >> 4) 
+	return ((((uint16_t)data << 8) | (crc >> 8)) ^ (uint8_t)(data >> 4)
 		^ ((uint16_t)data << 3));
 }
 
