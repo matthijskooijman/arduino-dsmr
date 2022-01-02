96a860a647c8626f80d2322abec6e0014596b879
Matthijs Kooijman <matthijs@stdin.nl>
2022-01-02 11:43:48 +0100

    Add a few comments

--- src/dsmr/parser.h
+++ src/dsmr/parser.h
@@ -212,14 +212,18 @@ struct NumParser {
     while(max_decimals--)
       value *= 10;
 
+    // If a unit was passed, check that the unit in the messages
+    // messages the unit passed.
     if (unit && *unit) {
       if (num_end >= end || *num_end != '*')
         return res.fail(F("Missing unit"), num_end);
       const char *unit_start = ++num_end; // skip *
       while(num_end < end && *num_end != ')' && *unit) {
+        // Next character in units do not match?
         if (*num_end++ != *unit++)
           return res.fail((const __FlashStringHelper*)INVALID_UNIT, unit_start);
       }
+      // At the end of the message unit, but not the passed unit?
       if (*unit)
         return res.fail((const __FlashStringHelper*)INVALID_UNIT, unit_start);
     }
